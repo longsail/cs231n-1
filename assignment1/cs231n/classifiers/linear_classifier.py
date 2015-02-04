fix gradient error with regularization

assignment1/cs231n/classifiers/linear_classifier.py
@@ -33,7 +33,6 @@ class LinearClassifier:
 
     # Run stochastic gradient descent to optimize W
     loss_history = []
-    # vel = np.zeros(self.W.shape)
     for it in xrange(num_iters):
       X_batch = None
       y_batch = None
@@ -52,10 +51,9 @@ class LinearClassifier:
       R = np.random.choice(X.shape[1], batch_size)
       X_batch = X[:,R]
       y_batch = y[R]
-
-      # ########################################################################
+      #########################################################################
       #                       END OF YOUR CODE                                #
-      # ########################################################################
+      #########################################################################
 
       # evaluate loss and gradient
       loss, grad = self.loss(X_batch, y_batch, reg)
@@ -67,9 +65,6 @@ class LinearClassifier:
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
       self.W -= learning_rate * grad
-
-      # vel = vel * 0.9 - learning_rate * grad
-      # self.W += vel
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################

assignment1/cs231n/classifiers/linear_svm.py
@@ -15,11 +15,12 @@ def svm_loss_naive(W, X, y, reg):
   - gradient with respect to weights W; an array of same shape as W
   """
   dW = np.zeros(W.shape) # initialize the gradient as zero
-  grad = np.zeros([W.shape[0], X.shape[1]])
 
   # compute the loss and the gradient
   num_classes = W.shape[0]
   num_train = X.shape[1]
+
+  grad_counter = np.zeros([num_classes, num_train])
   loss = 0.0
   for i in xrange(num_train):
     scores = W.dot(X[:, i])
@@ -30,13 +31,13 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
-        grad[y[i],i] -= 1
-        grad[j,i] += 1
+        grad_counter[j,i] += 1
+        grad_counter[y[i],i] -= 1
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
-  
+
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
 
@@ -48,10 +49,11 @@ def svm_loss_naive(W, X, y, reg):
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
-
-  dW = grad.dot(X.T)
-  dW /= num_train
   
+  dW = grad_counter.dot(X.T)
+  dW /= num_train
+  dW += reg * W
+
   return loss, dW
 
 
@@ -71,7 +73,7 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-
+  
   WX = W.dot(X)
   margin = WX - WX[y, np.arange(num_train)] + 1
   margin[y, np.arange(num_train)] = 0
@@ -84,7 +86,6 @@ def svm_loss_vectorized(W, X, y, reg):
   #                             END OF YOUR CODE                              #
   #############################################################################
 
-
   #############################################################################
   # TODO:                                                                     #
   # Implement a vectorized version of the gradient for the structured SVM     #
@@ -94,19 +95,16 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-
+  
   grad = np.zeros([W.shape[0], X.shape[1]])
-
   grad_wj = margin > 0
   grad_y = -1*sum(grad_wj)
-
   grad = grad + grad_wj
   grad[y, np.arange(num_train)] = grad_y
-  
   dW = grad.dot(X.T)
   dW /= num_train
-
-
+  dW += reg * W
+  
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################